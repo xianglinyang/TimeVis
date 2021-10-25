d791a0f91eb13d5c983c0817648cb3f20856675b
xianglin <yxlyzxl@gmail.com>
2021-10-25 21:37:53 +0800

    hierarchical index

--- singleVis/edge_dataset.py
+++ singleVis/edge_dataset.py
@@ -4,24 +4,29 @@ from PIL import Image
 
 # TODO two hierarchy index,
 class DataHandler(Dataset):
-    def __init__(self, edge_to, edge_from, feature_vector, transform=None):
+    def __init__(self, edge_to, edge_from, feature_vector, time_step, transform=None):
         self.edge_to = edge_to
         self.edge_from = edge_from
         self.data = feature_vector
-        self.transfrom = transform
+        self.time_step = time_step
+        self.transform = transform
 
     def __getitem__(self, item):
-        edge_to = self.edge_to[item]
-        edge_from = self.edge_from[item]
-        edge_to = self.data[edge_to]
-        edge_from = self.data[edge_from]
-        if self.transfrom is not None:
-            # TODO test this
+        time_step = item // self.time_step
+        index = item % self.time_step
+
+        edge_to = self.edge_to[time_step][index]
+        edge_from = self.edge_from[time_step][index]
+        edge_to = self.data[time_step][edge_to]
+        edge_from = self.data[time_step][edge_from]
+        if self.transform is not None:
+            # TODO correct or not?
             edge_to = Image.fromarray(edge_to)
-            edge_to = self.transfrom(edge_to)
+            edge_to = self.transform(edge_to)
             edge_from = Image.fromarray(edge_from)
-            edge_from = self.transfrom(edge_from)
+            edge_from = self.transform(edge_from)
         return edge_to, edge_from
 
     def __len__(self):
-        return len(self.data)
+        # return the number of all edges
+        return len(self.data*len(self.data[0]))
