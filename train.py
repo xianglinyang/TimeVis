aeea2983f637e782ad924ca5c3081d4b9f12600b
Yang Xianglin <yxlyzxl@gmail.com>
2021-12-30 16:58:48 +0800

    refactor

--- train.py
+++ train.py
@@ -2,7 +2,6 @@ import torch
 import sys
 import os
 import numpy as np
-import time
 import argparse
 
 from torch.utils.data import DataLoader
@@ -47,12 +46,12 @@ sys.path.append(content_path)
 from Model.model import *
 net = resnet18()
 classes = ("airplane", "car", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck")
-selected_idxs = np.random.choice(np.arange(LEN), size=LEN*DOWNSAMPLING_RATE//1, replace=False)
+
+selected_idxs = np.random.choice(np.arange(LEN), size=int(LEN*DOWNSAMPLING_RATE), replace=False)
 
 data_provider = DataProvider(content_path, net, 1, TIME_STEPS, 1, split=-1, device=DEVICE, verbose=1)
 # data_provider.initialize(LEN//10, l_bound=0.6)
 
-time_start = time.time()
 model = SingleVisualizationModel(input_dims=512, output_dims=2, units=256)
 negative_sample_rate = 5
 min_dist = .1
@@ -65,37 +64,20 @@ optimizer = torch.optim.Adam(model.parameters(), lr=.01, weight_decay=1e-5)
 lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=4, gamma=.1)
 
 edge_to, edge_from, probs, feature_vectors, attention = construct_spatial_temporal_complex(data_provider, selected_idxs, TIME_STEPS, NUMS, TEMPORAL_PERSISTENT, TEMPORAL_EDGE_WEIGHT)
-
 dataset = DataHandler(edge_to, edge_from, feature_vectors, attention)
 n_samples = int(np.sum(NUMS * probs) // 1)
-
 # chosse sampler based on the number of dataset
 if len(edge_to) > 2^24:
     sampler = CustomWeightedRandomSampler(probs, n_samples, replacement=True)
 else:
     sampler = WeightedRandomSampler(probs, n_samples, replacement=True)
-
 edge_loader = DataLoader(dataset, batch_size=1000, sampler=sampler)
-trainer = SingleVisTrainer(model, criterion, optimizer, edge_loader=edge_loader, DEVICE=DEVICE)
-patient = PATIENT
-for epoch in range(EPOCH_NUMS):
-    print("====================\nepoch:{}\n===================".format(epoch))
-    prev_loss = trainer.loss
-    loss = trainer.train_step()
-    # early stop, check whether converge or not
-    if prev_loss - loss < 1E-2:
-        if patient == 0:
-            break
-        else:
-            patient -= 1
-    else:
-        patient = PATIENT
 
-time_end = time.time()
-time_spend = time_end - time_start
-print("Time spend: {:.2f}".format(time_spend))
+trainer = SingleVisTrainer(model, criterion, optimizer, lr_scheduler,edge_loader=edge_loader, DEVICE=DEVICE)
+trainer.train(PATIENT, EPOCH_NUMS)
 trainer.save(save_dir=data_provider.model_path, file_name="SV")
 trainer.load(file_path=os.path.join(data_provider.model_path,"SV.pth"))
+
 
 ########################################################################################################################
 # evaluate
