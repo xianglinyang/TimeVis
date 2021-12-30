97e8775f4dbb46a2ad856d346e37515fe06740ad
Yang Xianglin <yxlyzxl@gmail.com>
2021-12-30 15:15:12 +0800

    update function to support visualizing tracjectory in different color

--- singleVis/trajectory.py
+++ singleVis/trajectory.py
@@ -1,48 +1,59 @@
-from matplotlib import scale
 import matplotlib.pyplot as plt
-import matplotlib as mpl
 import numpy as np
 
-def draw_trajectory(embeddings):
+def draw_trajectory(embeddings, save_path=None, x_min=None, x_max=None, y_min=None, y_max=None):
     fig = plt.figure()
     plt.quiver(embeddings[:-1, 0], embeddings[:-1, 1], embeddings[1:, 0]-embeddings[:-1, 0], embeddings[1:, 1]-embeddings[:-1, 1],scale_units='xy', angles='xy', scale=1)  
-    x_min = embeddings[:, 0].min()
-    x_max = embeddings[:, 0].max()
-    y_min = embeddings[:, 1].min()
-    y_max = embeddings[:, 1].max()
+    if x_min is None:
+        x_min = embeddings[:, 0].min()
+        x_max = embeddings[:, 0].max()
+        y_min = embeddings[:, 1].min()
+        y_max = embeddings[:, 1].max()
 
     plt.xlim((x_min-0.1*(x_max-x_min), x_max+0.1*(x_max-x_min)))
     plt.ylim((y_min-0.1*(y_max-y_min), y_max+0.1*(y_max-y_min)))
-    plt.savefig("/home/xianglin/projects/git_space/SingleVisualization/singleVis/trac.png")
+    if save_path is not None:
+        plt.savefig(save_path)
 
-def draw_two_trajectories(embeddings1, embeddings2):
-    fig = plt.figure()
-    ax = fig.add_subplot(121)
-    ax.quiver(embeddings1[:-1, 0], embeddings1[:-1, 1], embeddings1[1:, 0]-embeddings1[:-1, 0], embeddings1[1:, 1]-embeddings1[:-1, 1],scale_units='xy', angles='xy', scale=1)  
-    x_min = embeddings1[:, 0].min()
-    x_max = embeddings1[:, 0].max()
-    y_min = embeddings1[:, 1].min()
-    y_max = embeddings1[:, 1].max()
+def draw_two_trajectories(embeddings1, embeddings2, save_path=None, save_in_one=True):
 
-    ax.set_xlim((x_min-0.1*(x_max-x_min), x_max+0.1*(x_max-x_min)))
-    ax.set_ylim((y_min-0.1*(y_max-y_min), y_max+0.1*(y_max-y_min)))
-    ax.set_title("DVI")
+    x_min = min(embeddings1[:, 0].min(),embeddings2[:, 0].min())
+    x_max = max(embeddings1[:, 0].max(), embeddings2[:, 0].max())
+    y_min = min(embeddings1[:, 1].min(), embeddings2[:, 1].min())
+    y_max = max(embeddings1[:, 1].max(), embeddings2[:, 1].max())
+    if save_in_one:
+        fig = plt.figure()
+        fig, ax = plt.subplots(1, 1, figsize=(8, 8))
 
-    ax = fig.add_subplot(122)
-    ax.quiver(embeddings2[:-1, 0], embeddings2[:-1, 1], embeddings2[1:, 0]-embeddings2[:-1, 0], embeddings2[1:, 1]-embeddings2[:-1, 1],scale_units='xy', angles='xy', scale=1)  
-    x_min = embeddings2[:, 0].min()
-    x_max = embeddings2[:, 0].max()
-    y_min = embeddings2[:, 1].min()
-    y_max = embeddings2[:, 1].max()
+        plot1 = ax.quiver(embeddings1[:-1, 0], embeddings1[:-1, 1], embeddings1[1:, 0]-embeddings1[:-1, 0], embeddings1[1:, 1]-embeddings1[:-1, 1],scale_units='xy', angles='xy', scale=1, color='r')  
+        plot2 = ax.quiver(embeddings2[:-1, 0], embeddings2[:-1, 1], embeddings2[1:, 0]-embeddings2[:-1, 0], embeddings2[1:, 1]-embeddings2[:-1, 1],scale_units='xy', angles='xy', scale=1, color='b')  
 
-    ax.set_xlim((x_min-0.1*(x_max-x_min), x_max+0.1*(x_max-x_min)))
-    ax.set_ylim((y_min-0.1*(y_max-y_min), y_max+0.1*(y_max-y_min)))
-    ax.set_title("DVI-T")
+        ax.set_xlim((x_min-0.1*(x_max-x_min), x_max+0.1*(x_max-x_min)))
+        ax.set_ylim((y_min-0.1*(y_max-y_min), y_max+0.1*(y_max-y_min)))
+        ax.set_title("DVI vs DVI-T trajectory visualization")
+        ax.legend([plot1, plot2], ["DVI", "DVI-T"])
 
-    plt.savefig("/home/xianglin/projects/git_space/SingleVisualization/singleVis/trac.png")
-    
+    else:
+        fig = plt.figure()
+        ax = fig.add_subplot(121)
+        ax.quiver(embeddings1[:-1, 0], embeddings1[:-1, 1], embeddings1[1:, 0]-embeddings1[:-1, 0], embeddings1[1:, 1]-embeddings1[:-1, 1],scale_units='xy', angles='xy', scale=1)  
+
+        ax.set_xlim((x_min-0.1*(x_max-x_min), x_max+0.1*(x_max-x_min)))
+        ax.set_ylim((y_min-0.1*(y_max-y_min), y_max+0.1*(y_max-y_min)))
+        ax.set_title("DVI")
+
+        ax = fig.add_subplot(122)
+        ax.quiver(embeddings2[:-1, 0], embeddings2[:-1, 1], embeddings2[1:, 0]-embeddings2[:-1, 0], embeddings2[1:, 1]-embeddings2[:-1, 1],scale_units='xy', angles='xy', scale=1)  
+
+        ax.set_xlim((x_min-0.1*(x_max-x_min), x_max+0.1*(x_max-x_min)))
+        ax.set_ylim((y_min-0.1*(y_max-y_min), y_max+0.1*(y_max-y_min)))
+        ax.set_title("DVI-T")
+    if save_path is not None:
+        plt.savefig(save_path)
+
 
 if __name__ == "__main__":
+    # toy example
     embeddings_x = np.arange(5)
     embeddings_y = np.arange(2,7,1)
     embeddings_z = np.arange(-2, -7, -1)
@@ -50,8 +61,7 @@ if __name__ == "__main__":
     embeddings1 = embeddings1.transpose()
     embeddings2 = np.concatenate((embeddings_x[None,], embeddings_z[None,]), axis=0)
     embeddings2 = embeddings2.transpose()
-    # draw_trajectory(embeddings)
-
+    draw_trajectory(embeddings1)
     draw_two_trajectories(embeddings1, embeddings2)
     
 
