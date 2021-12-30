712fa3eb9322b61cbf3e3f55ee40a98378be324e
Yang Xianglin <yxlyzxl@gmail.com>
2021-12-30 15:26:11 +0800

    support visualize prev_data in trajectory

--- singleVis/visualizer.py
+++ singleVis/visualizer.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 import numpy as np
 import torch
-from torch._C import device
 
 class visualizer:
     def __init__(self, data_provider, sv_model, resolution, class_num, classes, cmap='tab10'):
@@ -34,7 +33,7 @@ class visualizer:
         # labels = prediction
         for c in range(self.class_num):
             color = self.cmap(c/(self.class_num-1))
-            plot = self.ax.plot([], [], '.', label=self.classes[c], ms=1,
+            plot = self.ax.plot([], [], '.', label=self.classes[c], ms=5,
                 color=color, zorder=2, picker=mpl.rcParams['lines.markersize'])
             self.sample_plots.append(plot[0])
 
@@ -42,14 +41,14 @@ class visualizer:
         for c in range(self.class_num):
             color = self.cmap(c/(self.class_num-1))
             plot = self.ax.plot([], [], 'o', markeredgecolor=color,
-                fillstyle='full', ms=3, mew=2.5, zorder=3)
+                fillstyle='full', ms=7, mew=2.5, zorder=3)
             self.sample_plots.append(plot[0])
 
         # labels != prediction, prediction stays inside of circle
         for c in range(self.class_num):
             color = self.cmap(c / (self.class_num - 1))
             plot = self.ax.plot([], [], '.', markeredgecolor=color,
-                                fillstyle='full', ms=2, zorder=4)
+                                fillstyle='full', ms=6, zorder=4)
             self.sample_plots.append(plot[0])
 
         # set the mouse-event listeners
@@ -120,7 +119,6 @@ class visualizer:
         diff = diff.reshape(-1, 1)
 
         color = color[:, 0:3]
-        # color = diff * 0.5 * color + (1 - diff) * np.ones(color.shape, dtype=np.uint8)
         color = diff * 0.5 * color + (1 - diff) * np.ones(color.shape, dtype=np.uint8)
         decision_view = color.reshape(resolution, resolution, 3)
         grid_view = grid.reshape(resolution, resolution, 2)
@@ -163,11 +161,8 @@ class visualizer:
             data = embedding[np.logical_and(pred == c, train_labels != pred)]
             self.sample_plots[2*self.class_num + c].set_data(data.transpose())
 
-        # if os.name == 'posix':
-        #     self.fig.canvas.manager.window.raise_()
-
-        self.fig.canvas.draw()
-        self.fig.canvas.flush_events()
+        # self.fig.canvas.draw()
+        # self.fig.canvas.flush_events()
 
         # plt.text(-8, 8, "test", fontsize=18, style='oblique', ha='center', va='top', wrap=True)
         plt.savefig(path)
@@ -190,11 +185,6 @@ class visualizer:
         desc = params_str % (self.resolution)
         self.desc.set_text(desc)
 
-        # train_data = self.data_provider.train_representation(epoch)
-        # train_labels = self.data_provider.train_labels(epoch)
-        # pred = self.data_provider.get_pred(epoch, train_data)
-        # pred = pred.argmax(axis=1)
-
         data = torch.from_numpy(data).to(device=self.data_provider.DEVICE, dtype=torch.float)
         embedding = self.model.encoder(data).cpu().detach().numpy()
         for c in range(self.class_num):
@@ -209,11 +199,51 @@ class visualizer:
             data = embedding[np.logical_and(pred == c, labels != pred)]
             self.sample_plots[2*self.class_num + c].set_data(data.transpose())
 
-        # if os.name == 'posix':
-        #     self.fig.canvas.manager.window.raise_()
+        # self.fig.canvas.draw()
+        # self.fig.canvas.flush_events()
+        plt.savefig(path)
 
-        self.fig.canvas.draw()
-        self.fig.canvas.flush_events()
+    
+    def savefig_trajectory(self, epoch, prev_data, prev_pred, prev_labels, data, pred, labels, path="vis"):
+        '''
+        Shows the current plot with given data
+        '''
+        self._init_plot()
 
-        # plt.text(-8, 8, "test", fontsize=18, style='oblique', ha='center', va='top', wrap=True)
+        x_min, y_min, x_max, y_max = self.get_epoch_plot_measures(epoch)
+
+        _, decision_view = self.get_epoch_decision_view(epoch, self.resolution)
+        self.cls_plot.set_data(decision_view)
+        self.cls_plot.set_extent((x_min, x_max, y_max, y_min))
+        self.ax.set_xlim((x_min, x_max))
+        self.ax.set_ylim((y_min, y_max))
+
+        params_str = 'res: %d'
+        desc = params_str % (self.resolution)
+        self.desc.set_text(desc)
+
+        data = torch.from_numpy(data).to(device=self.data_provider.DEVICE, dtype=torch.float)
+        embedding = self.model.encoder(data).cpu().detach().numpy()
+
+        prev_data = torch.from_numpy(prev_data).to(device=self.data_provider.DEVICE, dtype=torch.float)
+        prev_embedding = self.model.encoder(prev_data).cpu().detach().numpy()
+
+        all_labels = np.concatenate((prev_labels, labels), axis=0)
+        all_pred = np.concatenate((prev_pred, pred), axis=0)
+        all_embedding = np.concatenate((prev_embedding, embedding), axis=0)
+
+        # show data with prev_data
+        for c in range(self.class_num):
+            data = all_embedding[np.logical_and(all_labels == c, all_labels == all_pred)]
+            self.sample_plots[c].set_data(data.transpose())
+
+        for c in range(self.class_num):
+            data = all_embedding[np.logical_and(all_labels == c, all_labels != all_pred)]
+            self.sample_plots[self.class_num+c].set_data(data.transpose())
+
+        for c in range(self.class_num):
+            data = all_embedding[np.logical_and(all_pred == c, all_labels != all_pred)]
+            self.sample_plots[2*self.class_num + c].set_data(data.transpose())
+        
+        plt.quiver(prev_embedding[:, 0], prev_embedding[:, 1], embedding[:, 0]-prev_embedding[:, 0],embedding[:, 1]-prev_embedding[:, 1], scale_units='xy', angles='xy', scale=1)  
         plt.savefig(path)
