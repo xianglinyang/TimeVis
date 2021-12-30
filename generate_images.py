4869f9523f413b321c3e1200cd12545ca8cae5a7
Yang Xianglin <yxlyzxl@gmail.com>
2021-12-30 17:10:06 +0800

    update changes

--- generate_images.py
+++ generate_images.py
@@ -5,59 +5,51 @@ import numpy as np
 from torch.utils.data import DataLoader
 from torch.utils.data import WeightedRandomSampler
 from umap.umap_ import find_ab_params
-import time
 import json
 
 from singleVis.SingleVisualizationModel import SingleVisualizationModel
 from singleVis.losses import SingleVisLoss, UmapLoss, ReconstructionLoss
-from singleVis.edge_dataset import DataHandler
 from singleVis.trainer import SingleVisTrainer
 from singleVis.data import DataProvider
 from singleVis.visualizer import visualizer
 
-from singleVis.backend import fuzzy_complex, boundary_wise_complex, construct_step_edge_dataset, \
-    construct_temporal_edge_dataset, get_attention, construct_temporal_edge_dataset2
 import singleVis.config as config
 import argparse
+
 parser = argparse.ArgumentParser(description='Process hyperparameters...')
 parser.add_argument('--content_path', type=str)
-parser.add_argument('-d','--dataset', choices=['cifar10', 'mnist', 'fmnist','online'])
-
+parser.add_argument('-d','--dataset', choices=['online','cifar10', 'mnist', 'fmnist'])
+parser.add_argument('-p',"--preprocess", choice=[0,1], default=0)
 args = parser.parse_args()
 
 CONTENT_PATH = args.content_path
 DATASET = args.dataset
+PREPROCESS = args.preprocess
+
 LEN = config.dataset_config[DATASET]["TRAINING_LEN"]
 LAMBDA = config.dataset_config[DATASET]["LAMBDA"]
+DOWNSAMPLING_RATE = config.dataset_config[DATASET]["DOWNSAMPLING_RATE"]
+L_BOUND = config.dataset_config[DATASET]["L_BOUND"]
 
 # define hyperparameters
 
-DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+DEVICE = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
 EPOCH_NUMS = config.dataset_config[DATASET]["training_config"]["EPOCH_NUM"]
 TIME_STEPS = config.dataset_config[DATASET]["training_config"]["TIME_STEPS"]
 TEMPORAL_PERSISTENT = config.dataset_config[DATASET]["training_config"]["TEMPORAL_PERSISTENT"]
 NUMS = config.dataset_config[DATASET]["training_config"]["NUMS"]    # how many epoch should we go through for one pass
 PATIENT = config.dataset_config[DATASET]["training_config"]["PATIENT"]
+TEMPORAL_EDGE_WEIGHT = config.dataset_config[DATASET]["training_config"]["TEMPORAL_EDGE_WEIGHT"]
 
 content_path = CONTENT_PATH
 sys.path.append(content_path)
 
 from Model.model import *
 net = resnet18()
-classes = ("airplane", "car", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck")
-
 data_provider = DataProvider(content_path, net, 1, TIME_STEPS, 1, split=-1, device=DEVICE, verbose=1)
-
 model = SingleVisualizationModel(input_dims=512, output_dims=2, units=256)
-negative_sample_rate = 5
-min_dist = .1
-_a, _b = find_ab_params(1.0, min_dist)
-umap_loss_fn = UmapLoss(negative_sample_rate, DEVICE, _a, _b, repulsion_strength=1.0)
-recon_loss_fn = ReconstructionLoss(beta=1.0)
-criterion = SingleVisLoss(umap_loss_fn, recon_loss_fn, lambd=LAMBDA)
-optimizer = torch.optim.Adam(model.parameters(), lr=.01, weight_decay=1e-5)
 
-trainer = SingleVisTrainer(model, criterion, optimizer, edge_loader=None, DEVICE=DEVICE)
+trainer = SingleVisTrainer(model, criterion=None, optimizer=None, lr_scheduler=None, edge_loader=None, DEVICE=DEVICE)
 trainer.load(file_path=os.path.join(data_provider.model_path,"SV.pth"))
 
 ########################################################################################################################
@@ -81,6 +73,6 @@ for i in range(1, TIME_STEPS+1, 1):
 ########################################################################################################################
 # evaluate
 ########################################################################################################################
-# from singleVis.eval.evaluator import Evaluator
-# evaluator = Evaluator(data_provider, trainer)
-# evaluator.save_eval(n_neighbors=15, file_name="evaluation")
+from singleVis.eval.evaluator import Evaluator
+evaluator = Evaluator(data_provider, trainer)
+evaluator.save_eval(n_neighbors=15, file_name="evaluation")
