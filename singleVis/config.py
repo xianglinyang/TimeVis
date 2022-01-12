1db35bbe4dcd991fb30172661667019ba7bd4a48
Yang Xianglin <yxlyzxl@gmail.com>
2022-01-13 00:46:53 +0800

    add comments

--- singleVis/config.py
+++ singleVis/config.py
@@ -68,6 +68,9 @@ dataset_config = {
         "LAMBDA":20.,
         "DOWNSAMPLING_RATE": .1,
         "L_BOUND":0.5,
+        # "MAX_HAUSDORFF":.06, # for 10 epochs
+        # "ALPHA":2,
+        # "BETA":1.3,# larger, select less points from early epochs
         "MAX_HAUSDORFF":.06,
         "ALPHA":2,
         "BETA":1.3,
@@ -77,7 +80,7 @@ dataset_config = {
             "TIME_STEPS": 10,
             "TEMPORAL_PERSISTENT": 1,
             "NUMS": 5,   # the number of epochs to go through in one go
-            "PATIENT": 3,    # early stopping patient
+            "PATIENT": 4,    # early stopping patient
             "TEMPORAL_EDGE_WEIGHT":100,
         }
     }
