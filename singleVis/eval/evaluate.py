7ad58d2c4f5bb07ebc2e78a002170ef7b9f7c533
Yang Xianglin <yxlyzxl@gmail.com>
2022-03-09 19:29:14 +0800

    update bnn evaluator for faster calculation

--- singleVis/eval/evaluate.py
+++ singleVis/eval/evaluate.py
@@ -2,6 +2,7 @@
 Help functions to evaluate our visualization system
 """
 from sklearn.neighbors import KDTree
+from sklearn.neighbors import NearestNeighbors
 import numpy as np
 from scipy.stats import pearsonr
 from pynndescent import NNDescent
@@ -74,11 +75,14 @@ def evaluate_proj_boundary_perseverance_knn(data, embedding, high_centers, low_c
     :param n_neighbors: int, the number of neighbors
     :return boundary preserving property: float,boundary preserving property
     """
-    high_tree = KDTree(high_centers)
-    low_tree = KDTree(low_centers)
+    high_neigh = NearestNeighbors(n_neighbors=n_neighbors, radius=0.4)
+    high_neigh.fit(high_centers)
+    high_ind = high_neigh.kneighbors(data, n_neighbors=n_neighbors, return_distance=False)
 
-    _, high_ind = high_tree.query(data, k=n_neighbors)
-    _, low_ind = low_tree.query(embedding, k=n_neighbors)
+    low_neigh = NearestNeighbors(n_neighbors=n_neighbors, radius=0.4)
+    low_neigh.fit(low_centers)
+    low_ind = low_neigh.kneighbors(embedding, n_neighbors=n_neighbors, return_distance=False)
+
     border_pres = np.zeros(len(data))
     for i in range(len(data)):
         border_pres[i] = len(np.intersect1d(high_ind[i], low_ind[i]))
